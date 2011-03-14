7860de1563505b723186dbf53155f6423c2aafb6
Denis Malinovsky <dmalinovsky@gmail.com>
2011-03-14 15:07:25 +0600

    Packaged program using 'distribute'.

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+setup(
+    name='si-news',
+    version='0.1',
+    description='Tracks updates for zhurnal.lib.ru authors',
+    author='Denis Malinovsky',
+    author_email='dmalinovsky@gmail.com',
+    url='https://bitbucket.org/dmalinovsky/si-news/',
+    py_modules=['si_news'],
+    install_requires=['distribute', 'python>=2.6', 'pyquery'],
+    license='gpl',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: MacOS X',
+        'Environment :: Win32 (MS Windows)',
+        'Environment :: X11 Applications :: Qt',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: GNU General Public License (GPL)',
+        'Natural Language :: Russian',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Utilities',
+        ],
+    entry_points = {
+        'gui_scripts': [
+            'si_news = si_news',
+            ]
+        },
+)
